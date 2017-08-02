192bf09ef1b77424a8a645a685b7ca422f461000
haoge <470368500@qq.com>
2017-08-02 14:49:25 +0800

    remove try-catch from internal checker

--- support/src/main/java/com/lzh/datasupport/DataChecker.java
+++ support/src/main/java/com/lzh/datasupport/DataChecker.java
@@ -29,27 +29,20 @@ final class DataChecker {
     }
 
     @SuppressWarnings("unchecked")
-    private static boolean checkInternal(Object entity, List<Mapping> mappings) {
-        boolean success = true;
-        try {
-            for (Mapping mapping : mappings) {
-                ICheck[] checks = mapping.checks;
-                for (ICheck check : checks) {
-                    if (check == null) {
-                        continue;
-                    }
-                    Object value = mapping.field.get(entity);
-                    success = check.check(value, mapping.annotation);
-                    if (!success) {
-                        return false;
-                    }
+    private static boolean checkInternal(Object entity, List<Mapping> mappings) throws Exception {
+        for (Mapping mapping : mappings) {
+            ICheck[] checks = mapping.checks;
+            for (ICheck check : checks) {
+                if (check == null) {
+                    continue;
+                }
+                Object value = mapping.field.get(entity);
+                if (!check.check(value, mapping.annotation)) {
+                    return false;
                 }
             }
-        } catch (Exception e) {
-            success = false;
         }
-
-        return success;
+        return true;
     }
 
 }
